607632d37e403374c5478e76584048f1d5875298
asaiku <ash@MacBook-Air-Aleksandr.local>
2021-10-22 17:00:26 +0300

    learned params in func

---
+++ 012.params/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sliceOfInt := [][]int{{34, 35, 38}, {1, 2, 1}}
+	value := calculate(sliceOfInt...)
+	fmt.Println(value)
+}
+
+func calculate(numbers ...[]int) int {
+	result := 0
+
+	//fmt.Printf("%T", numbers)			// слайсы интов
+
+	for _, v := range numbers {
+		for _, v2 := range v {
+			result += v2
+		}
+	}
+
+	return result
+}
